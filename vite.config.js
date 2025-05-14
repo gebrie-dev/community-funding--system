6822fc05fde9904b7ac875b4f63e4367947a1bba
unknown <gabiwagnew@gmail.com>
2025-05-14 07:43:32 -0300

    admin routes fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-})
+  base: "/",
+  server: {
+    historyApiFallback: true,
+    middleware: [
+      (req, res, next) => {
+        if (req.url.startsWith("/admin")) {
+          req.url = "/index.html";
+        }
+        next();
+      },
+    ],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+});
